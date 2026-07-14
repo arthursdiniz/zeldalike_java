0e458b0e90ee77770cd8d1a6b49844814a788533
Arthur Sampaio Diniz <diniz.arthur@gmail.com>
2026-07-14 18:47:10 -0300

    Add player sprites, animation and rendering
    Load player sprite images and implement animated player entity. Entity.java gains sprite fields and direction state; Player.java now loads PNGs, sets defaults, handles movement input, sprite animation timing, and a draw(Graphics2D) method. GamePanel updated to use player.update() and player.draw(), and tileSize made public for use by entities. Also adds player sprite PNGs under res/player and updates IDE config (.idea/vcs.xml and modules.xml) to include the res module.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -13,7 +13,7 @@ public class GamePanel extends JPanel implements Runnable
     final int originalTileSize = 16;
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    final public int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;
@@ -94,23 +94,7 @@ public class GamePanel extends JPanel implements Runnable
 
     public void update(){
 
-        if(keyH.upPressed == true){
-
-            playerY -= playerSpeed;
-
-        }else if(keyH.downPressed == true){
-
-            playerY += playerSpeed;
-
-        }else if(keyH.leftPressed == true){
-
-            playerX -= playerSpeed;
-
-        }else if(keyH.rightPressed == true){
-
-            playerX += playerSpeed;
-
-        }
+       player.update();
 
     }
 
@@ -119,9 +103,7 @@ public class GamePanel extends JPanel implements Runnable
 
         Graphics2D g2 = (Graphics2D)g;
 
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX,playerY,tileSize,tileSize);
+        player.draw(g2);
 
         g2.dispose();
     }
